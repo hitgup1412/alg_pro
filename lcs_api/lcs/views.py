403b9120d19a483d015e97dc920a1d8b0d7bb6eb
hitgup1412 <82274100+hitgup1412@users.noreply.github.com>
2023-07-12 00:14:37 +0330

    Update views.py
    changing the post retrieving method

--- lcs_api/lcs/views.py
+++ lcs_api/lcs/views.py
@@ -1,26 +1,29 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+import os
 
 from .lcs import lcs_dna
+
 @api_view(['POST'])
 def lcs(request):
-    file1 = request.FILES['file1']
-    file2 = request.FILES['file2']
-    threshold = int(request.POST.get('threshold', 80))
+    file1_path = request.data.get('file1_path')
+    file2_path = request.data.get('file2_path')
+    threshold = int(request.data.get('threshold', 80))
 
-    seq1 = file1.read().decode('utf-8').strip()
-
+    # Read contents from file1
+    with open(file1_path, 'r') as file:
+        seq1 = file.read().strip()
+    
     results = []
-    with open('file3' , 'w') as file:
-        for line in file2:
-            seq2 = line.decode('utf-8').strip()
-            lcs = lcs_dna(seq1, seq2)
-            similarity_percentage = (len(lcs) / len(seq1)) * 100
+    with open('file3', 'w') as file:
+        with open(file2_path, 'r') as file2:
+            for line in file2:
+                seq2 = line.strip()
+                lcs = lcs_dna(seq1, seq2)
+                similarity_percentage = (len(lcs) / len(seq1)) * 100
 
-            if similarity_percentage >= threshold:
-                results.append({'person': seq1, 'parent': seq2, 'lcs': lcs})
-                file.write(f"{seq1}\t{seq2}\t{lcs}\n")
-        
-        
+                if similarity_percentage >= threshold:
+                    results.append({'person': seq1, 'parent': seq2, 'lcs': lcs})
+                    file.write(f"{seq1}\t{seq2}\t{lcs}\n")
 
     return JsonResponse({'results': results})
